8b1bbfdfa1929df3cfe63b9b16dff342b74d8da9
Michele Maccini <michelemaccini@gmail.com>
2016-12-02 16:22:37 +0100

    Correzione bugs e nuovo link

--- RadioTapeApp/src/main/java/com/mrpaloma/radiotape/NumberSerialKey.java
+++ RadioTapeApp/src/main/java/com/mrpaloma/radiotape/NumberSerialKey.java
@@ -4,6 +4,7 @@ package com.mrpaloma.radiotape;
  * Created by MicheleMaccini on 10/02/2015.
  */
 public class NumberSerialKey {
-    static String SENDER_ID_CLOUD_MESSAGE = "";
-    static String PASSWORD_ADMIN_STREAM = "";
+    static String SENDER_ID_CLOUD_MESSAGE = "589042602942";
+    //static String PASSWORD_ADMIN_STREAM = "delehawu9646";
+    static String PASSWORD_ADMIN_STREAM = "KcD2tk8Hk";
 }
